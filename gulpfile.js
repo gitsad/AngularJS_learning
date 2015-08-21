ec3d256992dfb0652c47518dfeffc8f2d77838c9
Marcin <marcin.sadowski95@outlook.com>
2015-08-21 12:46:18 +0200

    add bower, gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/*required methods */
+var gulp    = require('gulp');
+var concat  = require('gulp-concat');
+var rename  = require('gulp-rename');
+var uglify  = require('gulp-uglify');
+
+/*tasks */
+
+gulp.task('default', function()
+{
+    
+});
